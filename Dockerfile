a40970ecf0e68fdee7424ab568d26b0da4684756
Antonio Trenta <a.trenta@elsevier.com>
2020-09-25 18:23:05 +0100

    first

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# Build phase
+# the "as builder" sets the tag for this section
+FROM node:alpine as builder  
+WORKDIR '/app'
+COPY package.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+# Run phase
+# don't need to set a new tag here
+FROM nginx 
+COPY --from=builder /app/build /usr/share/nginx/html  
+# "--from" references the files in the container created in the referenced block
+# no need for a cmd; nginx base image has it set up already
--- src/App.js
+++ src/App.js
@@ -8,7 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Hi Antonio Trenta!
+        </p>
+        <p>
+          Your mother was a hamster and your father smelt of elderberries!
+        </p>
+        <p>
+          Quel mazzolin di fiori che vien dalla montagna. E bada ben che non si bagna che lo voglio regalar.
+        </p>
+        <p>
+          Oh Giorgio del Lago Maggiore, Oh Giorgio mi vuoi to portare ad Ascona, ad Ascona sul Lago Maggiore?
         </p>
         <a
           className="App-link"
